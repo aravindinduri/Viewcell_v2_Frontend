17ff2ca2ffc861c59c0ee647c77976a7007143e3
ARAVIND INDURI <thearavindinduri@gmail.com>
2025-01-31 20:08:57 +0530

    fixed version

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     server: {
         proxy: {
-            "/api": "https://video-streaming-app-wme1.onrender.com/api/v1",
+            "/api": "https://viewcell-v2-backend.onrender.com/api/v1",
         },
     },
     plugins: [react()],

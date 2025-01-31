ebc9caea606d71ba82e148d9dd3dc9525042d67e
ARAVIND INDURI <thearavindinduri@gmail.com>
2025-01-31 20:34:07 +0530

    added changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+    root: "src", 
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+    },
     server: {
         proxy: {
             "/api": "https://viewcell-v2-backend.onrender.com/api/v1",
         },
     },
     plugins: [react()],
-    build: {
-        outDir: '../dist',
-    },
 });
